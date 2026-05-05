a06c714c74702a147b40b0445095346fa62e703b
mokim <mamokim2005@gmail.com>
2026-05-05 23:46:59 +0600

    added project layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,29 @@
-import Image from "next/image";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-export default function Home() {
+export const metadata = {
+  title: "My App",
+  description: "Next.js App",
+};
+
+export default function RootLayout({ children }) {
   return (
- <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
- </div>);
+    <html lang="en">
+      <body className="flex flex-col min-h-screen">
+        
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Main Content */}
+        <main className="flex-grow container mx-auto px-4 py-6">
+          {children}
+        </main>
+
+        {/* Footer */}
+        <Footer />
+
+      </body>
+    </html>
+  );
 }
